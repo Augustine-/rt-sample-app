e113da7237f696b667d26feaa0f26fb6dd9aba28
Augustine Wittkower <augustine.wittkower@gmail.com>
2016-01-23 15:53:34 -0800

    add uniqueness validation to database and model, add length and presence validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
             format: {  with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
+  validates :username, presence: true, length: { maximum: 16 },
+            uniqueness: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160121233317) do
+ActiveRecord::Schema.define(version: 20160123062356) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -48,8 +48,10 @@ ActiveRecord::Schema.define(version: 20160121233317) do
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
